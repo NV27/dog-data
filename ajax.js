de4eb392a9058b4c65e77f5ee49e78dce8604ab3
Louis <louisprotano@hotmail.com>
2024-04-12 17:29:42 +0100

    working with button to change untis

--- ajax.js
+++ ajax.js
@@ -1,18 +1,48 @@
-fetch("doggies.json").then(response => {
-    return response.json()
-}).then(data => {
-    console.log(data);
-    d = data.data
-    //document.querySelector('.content').innerHTML += `<ul>`
-    for(i=0; i < d.length; i++){
-        document.querySelector('.content').innerHTML += `
-        <div class="dog">
-            <h1>Name: ${d[i].name}</h1>
-            <p>Weight: ${d[i].weight.imperial}</p>
-        </div>
-        `
-            
-            
+var pageUnits = true;
+loadJson();
+
+document.querySelector(".unit-button").addEventListener("click",() => {
+    pageUnits = !pageUnits;
+    console.log(pageUnits);
+    if (!pageUnits){
+        document.querySelector(".unit-button").textContent = "metric"
     }
-    //document.querySelector('.content').innerHTML += `</ul>`
+    else{
+        document.querySelector(".unit-button").textContent = "imperial"
+    }
+    loadJson();
 })
+
+function loadJson(){
+    fetch("doggies.json").then(response => {
+        return response.json()
+    }).then(data => {
+        console.log(data);
+        d = data.data
+        document.querySelector('.content').innerHTML = ""
+        for(i=0; i < d.length; i++){
+    
+            if (pageUnits == true){
+                h = `Height: ${d[i].height.imperial} inches`;
+                w = `Weight: ${d[i].weight.imperial} pounds`;
+            }
+            else{
+                h = `Height: ${d[i].height.metric} cm`;
+                w = `Weight: ${d[i].weight.metric} kilograms`;
+            }
+            
+            document.querySelector('.content').innerHTML += `
+            <div class="dog">
+                <h2>Name: ${d[i].name}</h2>
+                <p>Breed Group: ${d[i].breed_group}</p>
+                <p>Bred For: ${d[i].bred_for}</p>
+                <p>Lifespan: ${d[i].life_span}</p>
+                <p>Temperament: ${d[i].temperament}</p>
+                <p>${h} </p>
+                <p>${w} </p>
+            </div>`
+        }
+    })
+}
+
+
