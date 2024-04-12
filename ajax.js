714ee5bf7143e23f0bced46c5a9ecd59668c2d06
Louis <louisprotano@hotmail.com>
2024-04-12 16:26:28 +0100

    styling

--- ajax.js
+++ ajax.js
@@ -1,5 +1,18 @@
-fetch(doggies.json).then(response => {
+fetch("doggies.json").then(response => {
     return response.json()
 }).then(data => {
     console.log(data);
+    d = data.data
+    //document.querySelector('.content').innerHTML += `<ul>`
+    for(i=0; i < d.length; i++){
+        document.querySelector('.content').innerHTML += `
+        <div class="dog">
+            <h1>Name: ${d[i].name}</h1>
+            <p>Weight: ${d[i].weight.imperial}</p>
+        </div>
+        `
+            
+            
+    }
+    //document.querySelector('.content').innerHTML += `</ul>`
 })
